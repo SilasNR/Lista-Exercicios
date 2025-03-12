7664dbfe08a5e5cbaafe9706caf4898f6a54824e
Silas Rodrigues <silasnas2018@gmail.com>
2025-03-12 17:30:42 -0300

    Lista2b iniciada

---
+++ Lista/src/main/java/com/mycompany/lista2/Lista2b.java
@@ -0,0 +1,24 @@
+package com.mycompany.lista2;
+
+import java.util.*;
+
+/*
+ * b) Tendo como entrada a altura e o sexo de uma pessoa, 
+ * construa um programa que exibe o peso ideal para ela, em quilos. 
+ * Use as seguintes fórmulas (adote ℎ como a altura e em metros):
+ *• Homens: 72,7 × ℎ − 58
+ *• Mulheres: 62,1 × ℎ − 44,7
+ */
+
+public class Lista2b {
+    public static void main(String[] args) {
+        float altura, sexo, peso;
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Digite a altura :");
+        altura = sc.nextFloat();
+
+        System.out.println("Digite o sexo \n1 -");
+    }
+}
